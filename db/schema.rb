3e268b2a00c867c12e22309adc2517f72afc6661
Marcus Richard <msrichard18@gmail.com>
2016-10-17 15:30:23 -0400

    deleted size column from districts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017181926) do
+ActiveRecord::Schema.define(version: 20161017192701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20161017181926) do
 
   create_table "districts", force: :cascade do |t|
     t.string   "name",           null: false
-    t.string   "size"
     t.string   "representative", null: false
     t.integer  "city_id",        null: false
     t.datetime "created_at",     null: false
