aa9561bc8a4c3ed493d947be706bd0f552e4e512
Marcus Richard <msrichard18@gmail.com>
2016-10-22 14:39:57 -0400

    seeded db with an address

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+address1 = Address.create(street: '50 Bothfeld Rd', city: 'Newton', state: 'MA', zip_code: '02459', district_id: 4)
